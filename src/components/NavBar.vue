fa978d3c80bd81e503a3ca0a8fe6ec105bd7ccd2
beamedeiros <beatriz.santos@prima.com.br>
2023-09-05 21:43:55 -0300

    POP-26 color page

---
+++ src/components/NavBar.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div>
+        teste
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import NavBar from '@/components/NavBar.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <nav-bar/>
 </template>
