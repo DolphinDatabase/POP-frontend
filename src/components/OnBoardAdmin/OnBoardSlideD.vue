617283bb51a9fba586d0c7fa01610d7ad1e361b7
beamedeiros <beatriz.santos@prima.com.br>
2023-10-21 22:50:01 -0300

    POP-61 - atualização onboard

--- src/components/OnBoardAdmin/OnBoardSlideD.vue
+++ src/components/OnBoardAdmin/OnBoardSlideD.vue
@@ -1,38 +1,36 @@
 <script setup lang="ts">
-const props = defineProps({
-  fecharOnBoard: {
-    required: true,
-    type: Function
-  }
-})
+import { ref } from 'vue'
+import OnBoardItem from '@/components/OnBoard/OnBoardItem.vue'
+
+const items = ref(['Adicionar', 'Editar', 'Excluir parâmetros'])
 </script>
 <template>
   <div class="on-board-d">
-    <h2>Acesse essa tela <span>a qualquer momento</span>, clicando em ajuda</h2>
-    <img src="../../assets/slides/adm_help.svg" alt="" />
-    <el-button type="primary" round @click="props.fecharOnBoard">Ir para a aplicação</el-button>
+    <h4>Nos termos do proprietário clicando em "continuar" você consegue:</h4>
+    <div class="on-board-item-d">
+      <OnBoardItem v-for="(item, index) in items" :key="index" :texto="item" />
+    </div>
+    <h6>Que serão utilizados para facilitar a transparência e proteção ao usuário.</h6>
+    <img src="../../assets/slides/param.svg" alt="" style="width: 700px;"/>
   </div>
 </template>
 
 <style scoped>
-.on-board-d {
-  display: grid;
-  gap: 32px;
-  justify-items: center;
-  align-items: center;
-}
-.on-board-d h2 {
+.on-board-d h4 {
   color: #405261;
   padding: 16px;
 }
 
-.on-board-d span {
-  color: #60bb45;
+.on-board-d h6 {
+  color: #748494;
+  text-align: left;
+  font-weight: 200;
+  font-size: medium;
 }
-</style>
 
-<style>
-.on-board-d .el-button.el-button--primary.is-round {
-  width: 300px;
+.on-board-item-d {
+  display: grid;
+  gap: 16px;
+  margin-bottom: 32px;
 }
 </style>
